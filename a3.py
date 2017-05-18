0bbd7ee61b7ce57cac6ff7c02301cf2b5a3790db
LaVieEstDure <mishraraghav24@gmail.com>
2017-05-18 12:28:02 +1000

    Updated support code to v1.0.2

--- a3.py
+++ a3.py
@@ -4,16 +4,19 @@ Semester 1, 2017
 """
 
 import tkinter as tk
-from tkinter import messagebox
 
 import model
 import view
 from game_regular import RegularGame
+# # For alternative game modes
+# from game_make13 import Make13Game
+# from game_lucky7 import Lucky7Game
+# from game_unlimited import UnlimitedGame
 
 __author__ = "Raghav Mishra"
-__email__ = "<r.mishra@uqconnect.edu.au>"
+__email__ = "r.mishra@uqconnect.edu.au"
 
-__version__ = "1.0.0"
+__version__ = "1.0.2"
 
 
 # Once you have created your basic gui (LoloApp), you can delete this class
@@ -89,11 +92,15 @@ class BaseLoloApp:
         return runner
 
     def activate(self, position):
-        """Attempts to activate tile at a given position.
+        """Attempts to activate the tile at the given position.
 
         Parameters:
             position (tuple<int, int>): Row-column position of the tile.
-            """
+
+        Raises:
+            IndexError: If position cannot be activated.
+        """
+        # Magic. Do not touch.
         if position is None:
             return
 
@@ -103,9 +110,9 @@ class BaseLoloApp:
         if position in self._game.grid:
 
             if not self._game.can_activate(position):
-                return messagebox.showwarning("Can't Activate",
-                                              "Cannot activate position {}"
-                                              .format(position))
+                hell = IndexError("Cannot activate position {}".format(position
+                                                                       ))
+                raise hell  # he he
 
             def finish_move():
                 self._grid_view.draw(self._game.grid,
@@ -119,13 +126,40 @@ class BaseLoloApp:
                                               callback=finish_move)
             animation()
 
+    def remove(self, *positions):
+        """Attempts to remove the tiles at the given positions.
+
+        Parameters:
+            *positions (tuple<int, int>): Row-column position of the tile.
+
+        Raises:
+            IndexError: If position cannot be activated.
+        """
+        if len(positions) is None:
+            return
+
+        if self._game.is_resolving():
+            return
+
+        def finish_move():
+            self._grid_view.draw(self._game.grid,
+                                 self._game.find_connections())
+
+        def draw_grid():
+            self._grid_view.draw(self._game.grid)
+
+        animation = self.create_animation(self._game.remove(*positions),
+                                          func=draw_grid,
+                                          callback=finish_move)
+        animation()
+
     def reset(self):
         """Resets the game."""
         raise NotImplementedError("Abstract method")
 
     def game_over(self):
         """Handles the game ending."""
-        raise NotImplementedError("Abstract method")
+        raise NotImplementedError("Abstract method")  # no mercy for stooges
 
     def score(self, points):
         """Handles increase in score."""
@@ -134,30 +168,17 @@ class BaseLoloApp:
         # raise NotImplementedError("Abstract method")
         # But so that the game can work prior to this method being implemented,
         # we'll just print some information.
+        # Sometimes I believe Python ignores all my comments :(
         print("Scored {} points. Score is now {}.".format(points,
                                                           self._game.get_score(
                                                           )))
         print("Don't forget to override the score method!")
 
 
-class LoloApp(BaseLoloApp):
-    """GUI class for LoloApp. Inherits from BaseLoloApp.
-    """
-    # def __init__(self, arg):
-    #     """Constructor
-    #
-    #     Parameters:
-    #         master (tk.Tk|tk.Frame): The parent widget.
-    #         game (model.AbstractGame): The game to play. Defaults to a
-    #                                    game_regular.RegularGame.
-    #         grid_view (view.GridView): The view to use for the game. Optional.
-    #
-    #     Raises:
-    #         ValueError: If grid_view is supplied, but game is not.
-    #     """
-    #     pass
-    pass
 # Define your classes here
+class LoloApp(BaseLoloApp):
+    """GUI class for Lolo"""
+    pass
 
 
 def main():
--- play_game.py
+++ play_game.py
@@ -11,16 +11,16 @@ import game_unlimited
 __author__ = "Benjamin Martin and Brae Webb"
 __copyright__ = "Copyright 2017, The University of Queensland"
 __license__ = "MIT"
-__version__ = "1.0.0"
+__version__ = "1.0.2"
 
 
 def main():
     """Plays a game."""
 
-    # game = game_regular.RegularGame()
+    game = game_regular.RegularGame()
     # game = game_make13.Make13Game()
-    # game = game_lucky7.Lucky7Game(lucky_value=5)
-    game = game_unlimited.UnlimitedGame()
+    # game = game_lucky7.Lucky7Game()
+    # game = game_unlimited.UnlimitedGame()
 
     root = tk.Tk()
     app = a3.LoloApp(root, game)
@@ -29,3 +29,5 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
